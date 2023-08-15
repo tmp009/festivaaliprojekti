28d4df4a35d119c3e0cf6eee4085a5ff1f2937fe
Sagir <s2200767@edu.bc.fi>
2023-08-15 12:57:52 +0300

    created some sql queries

--- test.sql
+++ test.sql
@@ -1,7 +1,15 @@
 
-
+-- search artist, city, festival
 select id, nimi, maa, kuvaus from artisti where artisti.nimi like ?;
 
 select id, nimi from kaupunki where kaupunki.nimi like ?;
 
 select id, nimi, aika, paikka, osallistujat from festivaali where festivaali.nimi like ?;
+
+
+-- city where artist appears
+
+select artisti.nimi as artisti, kaupunki.nimi as kaupunki from artisti, osallistuja, kaupunki, festivaali
+where osallistuja.artisti=artisti.id 
+and festivaali.id=osallistuja.festivaali 
+and festivaali.paikka=kaupunki.id;
